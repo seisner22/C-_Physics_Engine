95e0efc18171bff638e1662f8478fc42aa28c9c9
aniket <raniketd@gmail.com>
2020-12-18 19:37:19 -0500

    added objects.hpp, which has Sphere, will finish Prism today

---
+++ objects.hpp
@@ -0,0 +1,16 @@
+class Sphere {
+    public:
+        float radius;
+        struct vec4 {
+            struct vec3;
+            float m;
+        };
+        struct vec3 {
+            float x, y, z;
+        };
+};
+
+class Prism {
+    public:
+        ;
+};
